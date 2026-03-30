159c36b86b8c28bba1a20e2c87777375b2d0d20f
LessGoK <kgwelelesego10@gmail.com>
2026-03-30 04:22:00 +0200

    added an api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+function updateWeatherDetails(response) {
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function fetchData(city) {
+  let apiKey = "ba9bc3d0b8ta33o052590808f264424e";
+
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios(apiUrl).then(updateWeatherDetails);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInput.value;
+
+  fetchData(searchInput.value);
 }
 
 let formElement = document.querySelector("#search-form");
 formElement.addEventListener("submit", handleSearchSubmit);
+
+fetchData("Los Angeles");
